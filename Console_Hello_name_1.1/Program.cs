ed94a371ecba092f66a8600154b3b63edcbe92cd
Shubham Maurya <Shubham_Maurya@epam.com>
2023-04-28 20:59:59 +0530

    added first and second task

---
+++ Console_Hello_name_1.1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enter your name : ");
+string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name);
+Console.ReadKey();
---
+++ HelloNameForm/Form1.cs
@@ -0,0 +1,26 @@
+namespace HelloNameForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Name = textBox1.Text;
+            MessageBox.Show("Hello, " +Name);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
